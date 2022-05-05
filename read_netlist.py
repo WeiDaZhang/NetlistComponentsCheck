263bb790d0b7029c10dc3501cd893bf2518ac8dd
Weida Zhang <weidazhang@physics.ox.ac.uk>
2022-05-05 16:02:16 +0100

    add read netlist file

---
+++ read_netlist.py
@@ -0,0 +1,21 @@
+"""
+Read netlist file
+Performed on 2022 May 05
+"""
+
+import socket as skt
+import time
+import math
+import os
+
+HOST = '192.168.1.32'
+PORT = 1234
+
+def connect():
+
+def main():
+    connect()
+        
+
+if __name__ == '__main__':
+    main()
